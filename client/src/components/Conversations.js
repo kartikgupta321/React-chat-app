7098280ff2b5dae9c6544328d67ef523dde4dd8e
kartikgupta <kartik.gupta2505@gmail.com>
2022-07-22 19:57:57 +0530

    conversation panel
    conversation error panel crossed

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
+import { useContacts } from '../contexts/ContactsProvider';
+import { useConversations } from '../contexts/ConversationsProvider';
 
 export default function Conversations() {
+  const { conversations} = useConversations()
+  const contacts = useContacts()
+
   return (
-    <div>Conversations</div>
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item
+          key={index}
+        >
+          {conversation.recipients.map(r => r.name).join(', ')}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   )
 }
-
--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect, useCallback } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage';
+import { useContacts } from './ContactsProvider';
 
 const ConversationsContext = React.createContext()
 
@@ -9,15 +10,54 @@ export function useConversations() {
 
 export function ConversationsProvider({ id, children }) {
   const [conversations, setConversations] = useLocalStorage('conversations', [])
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
+  const { contacts } = useContacts()
 
   function createConversation(recipients) {
     setConversations(prevConversations => {
+      // const names = recipients.map(r=>{
+      //   const contact = contacts.find(contact=>{
+      //     return contact.id = r
+      //   })
+      //   const name = (contact && contact.name) || r
+      //   return name
+      // })
       return [...prevConversations, { recipients, messages: [] }]
     })
   }
+  
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map(recipient => {
+      const contact = contacts.find(contact => {
+        return contact.id === recipient
+      })
+      const name = (contact && contact.name) || recipient
+      return { id: recipient, name }
+    })
+
+    const messages = conversation.messages.map(message => {
+      const contact = contacts.find(contact => {
+        return contact.id === message.sender
+      })
+      const name = (contact && contact.name) || message.sender
+      const fromMe = id === message.sender
+      return { ...message, senderName: name, fromMe }
+    })
+    
+    const selected = index === selectedConversationIndex
+    return { ...conversation, messages, recipients, selected }
+  })
+  
+  const value = {
+    conversations: formattedConversations,
+    // selectedConversation: formattedConversations[selectedConversationIndex],
+    // sendMessage,
+    // selectConversationIndex: setSelectedConversationIndex,
+    createConversation
+  }
 
   return (
-    <ConversationsContext.Provider value={{ conversations, createConversation }}>
+    <ConversationsContext.Provider value={value}>
       {children}
     </ConversationsContext.Provider>
   )
